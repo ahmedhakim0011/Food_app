9262f71bc129e58938a0a19fc672bf783108bba0
Ahmed Hakim Ali <ahmdahmed259@gmail.com>
2022-03-01 22:35:54 +0500

    revview cart page done

---
+++ lib/model/productModel.dart
@@ -0,0 +1,10 @@
+class productModel {
+  String productName;
+  String productImage;
+  int productPrice;
+  productModel({
+    required this.productName,
+    required this.productImage,
+    required this.productPrice,
+  });
+}
